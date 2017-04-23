c15bdef427d95d4962325dac2463fe73249fbbc7
pawel <218392@student.pwr.edu.pl>
2017-04-23 16:39:36 +0200

    #12 logowanie, #14 rejestracja

--- mobile/src/main/java/com/example/wearlearn/SignUpActivity.java
+++ mobile/src/main/java/com/example/wearlearn/SignUpActivity.java
@@ -98,7 +98,7 @@ public class SignUpActivity extends AppCompatActivity {
         _signupButton.setEnabled(true);
     }
 
-    public boolean validate() {
+    private boolean validate() {
         boolean valid = true;
 
         String name = _nameText.getText().toString();
@@ -109,32 +109,26 @@ public class SignUpActivity extends AppCompatActivity {
         if (name.isEmpty() || name.length() < 3) {
             _nameText.setError("at least 3 characters");
             valid = false;
-        } else {
-            _nameText.setError(null);
         }
-
 
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             _emailText.setError("enter a valid email address");
             valid = false;
-        } else {
-            _emailText.setError(null);
         }
 
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
-        } else {
-            _passwordText.setError(null);
+        }  else if (android.util.Patterns.EMAIL_ADDRESS.matcher(password).matches()){
+            _passwordText.setError("email address cannot be a password");
+            valid = false;
         }
 
         if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
-            _reEnterPasswordText.setError("Password Do not match");
+            _reEnterPasswordText.setError("Passwords do not match");
             valid = false;
-        } else {
-            _reEnterPasswordText.setError(null);
         }
 
         return valid;
